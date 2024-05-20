2c2c481246a35f9e872594f1435e53e501629c7f
Catmoll1013 <168452129+Catmoll1013@users.noreply.github.com>
2024-05-20 00:31:59 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -5,11 +5,20 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('netflix_titles.csv', encoding='latin-1')
 title_types = df['type'].value_counts()
 
-fig, ax = plt.subplots(figsize=(10, 6))
+fig1, ax1 = plt.subplots(figsize=(10, 6))
 
 title_types.plot(kind='pie', colors=['blue', 'pink'], ax=ax)
-ax.set_ylabel('')  # Remover o rótulo do eixo Y
-ax.set_title('Contagem de Títulos na Netflix por Tipo')
-ax.tick_params(axis='x', rotation=0)
+ax1.set_ylabel('')  # Remover o rótulo do eixo Y
+ax1.set_title('Contagem de Títulos na Netflix por Tipo')
+ax1.tick_params(axis='x', rotation=0)
+st.pyplot(fig1)
 
-st.pyplot(fig)
+title_types = df['type'].value_counts()
+
+fig2, ax2 = plt.subplots(figsize=(10, 6))
+
+title_types.plot(kind='bars', colors=['blue', 'pink'], ax=ax)
+ax2.set_ylabel('')  # Remover o rótulo do eixo Y
+ax2.set_title('Contagem de Títulos na Netflix por Tipo')
+ax2.tick_params(axis='x', rotation=0)
+st.pyplot(fig2)
