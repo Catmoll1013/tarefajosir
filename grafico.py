c9ffbb152a69be99473f7a1d7a0b10fb56a6d2df
Catmoll1013 <168452129+Catmoll1013@users.noreply.github.com>
2024-05-20 00:14:34 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,15 +1,16 @@
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('netflix_titles.csv', encoding='latin-1')
 
-
 title_types = df['type'].value_counts()
+
+plt.figure(figsize=(10, 6))  # Definir tamanho do gráfico
 title_types.plot(kind='bar', color=['blue', 'pink'])
 plt.xlabel('Tipo de Título')
 plt.ylabel('Contagem')
 plt.title('Contagem de Títulos na Netflix por Tipo')
 plt.xticks(rotation=0)
-plt.figure(figsize=(10, 6))  
 
-plt.show()
+st.pyplot()
