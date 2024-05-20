c19dac45fcb94ad2823716b621578d1c58e52c5b
Catmoll1013 <168452129+Catmoll1013@users.noreply.github.com>
2024-05-20 00:19:58 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -7,7 +7,7 @@ df = pd.read_csv('netflix_titles.csv', encoding='latin-1')
 title_types = df['type'].value_counts()
 
 fig, ax = plt.subplots(figsize=(10, 6))  # Definir tamanho do gráfico
-title_types.plot(kind='bar', color=['blue', 'pink'], ax=ax)
+title_types.plot(kind='pie', color=['blue', 'pink'], ax=ax)
 ax.set_xlabel('Tipo de Título')
 ax.set_ylabel('Contagem')
 ax.set_title('Contagem de Títulos na Netflix por Tipo')
